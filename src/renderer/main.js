f1a245802be0e1981d9d3a845293e1de0dd8f26c
jvens <jeramie.vens@gmail.com>
2018-07-01 12:39:09 -0500

    Add firebase support

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -1,5 +1,12 @@
+/* eslint-disable */
+
 import Vue from 'vue'
 import axios from 'axios'
+
+import VueFire from 'vuefire'
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/storage'
 
 import App from './App'
 import router from './router'
@@ -8,6 +15,21 @@ import store from './store'
 if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
 Vue.http = Vue.prototype.$http = axios
 Vue.config.productionTip = false
+
+Vue.use(VueFire)
+firebase.initializeApp({
+    apiKey: "AIzaSyDQOhAqHtOOjWetSbzfvdlpXB_RMtfb_NU",
+    authDomain: "firstinspiresiowa2018.firebaseapp.com",
+    databaseURL: "https://firstinspiresiowa2018.firebaseio.com",
+    projectId: "firstinspiresiowa2018",
+    storageBucket: "firstinspiresiowa2018.appspot.com",
+    messagingSenderId: "42095702430"
+})
+const firestore = firebase.firestore()
+firestore.settings({timestampsInSnapshots: true})
+export const db = firestore
+const firestorage = firebase.storage()
+export const storage = firestorage
 
 /* eslint-disable no-new */
 new Vue({
